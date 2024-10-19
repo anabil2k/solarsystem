4aa37abefd49ccafe0fcf35923a44c4f8653ceb0
Ahmed Nabil Zin EL-Din <a_nabil2k@hotmail.com>
2024-10-19 19:23:19 +0300

    cd 131

--- app-test.js
+++ app-test.js
@@ -145,17 +145,17 @@ describe('Planets API Suite', () => {
         //       });
         // });
 
-        // it('it should return an error for an invalid planet id', (done) => {
-        //     let payload = { id: 999 }; // Invalid id
-        //     chai.request(server)
-        //         .post('/planet')
-        //         .send(payload)
-        //         .end((err, res) => {
-        //             res.should.have.status(200);
-        //             res.text.should.eql("Error in Planet Data");
-        //             done();
-        //         });
-        // });
+        it('it should return an error for an invalid planet id', (done) => {
+            let payload = { id: 999 }; // Invalid id
+            chai.request(server)
+                .post('/planet')
+                .send(payload)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.text.should.eql("Error in Planet Data");
+                    done();
+                });
+        });
         
 
     });        
@@ -199,18 +199,18 @@ describe('Testing Other Endpoints', () => {
         });
     });
 
-    describe('MongoDB Connection', () => {
-        it('should handle MongoDB connection error', (done) => {
-            mongoose.connect("invalid_uri", {
-                user: "invalid_user",
-                pass: "invalid_pass",
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            }, function(err) {
-                err.should.exist;
-                done();
-            });
-        });
-    });
+    // describe('MongoDB Connection', () => {
+    //     it('should handle MongoDB connection error', (done) => {
+    //         mongoose.connect("invalid_uri", {
+    //             user: "invalid_user",
+    //             pass: "invalid_pass",
+    //             useNewUrlParser: true,
+    //             useUnifiedTopology: true
+    //         }, function(err) {
+    //             err.should.exist;
+    //             done();
+    //         });
+    //     });
+    // });
     
 });
